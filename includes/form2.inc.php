b897c946390d5c0274f52c424aa46c6a52950650
ChocoIa <steevendepro@gmail.com>
2022-05-05 10:21:07 +0200

    Modif form2

--- includes/form2.inc.php
+++ includes/form2.inc.php
@@ -1,8 +1,43 @@
+<h1 class="text-center">Ajouter plus de données</h1>
+<form  action="index.php" method="POST">
 <div class="row">
 <div class="card col-md-7 mx-auto my-1">
-<?php 
-include("includes/form.inc.html");
-?>
+<div class="form-group">
+        <div class="form-floating mb-3">
+          <input type="Prénom" class="form-control" name="prénom" id="Form-Prénom" placeholder="Prénom" required>
+          <label for="floatingPrénom">Prénom</label>
+        </div>
+    
+        <div class="form-floating">
+          <input type="Nom" class="form-control"name="nom" id="Form-Nom" placeholder="Nom" required>
+          <label for="floatingNom">Nom</label>
+        </div>
+      </div>
+    
+        <div class="form-group">
+        <label for="Age" class="form-label mt-4">Age (18 à 70 ans) </label>
+            <input type="number" min="18" max="70" class="form-control" name="age" id="form-age" placeholder="Renseignez votre age" required>
+        </div>
+    
+        <div class="form-group">
+            <div class="input-group mb-3 mt-4">
+                <span class="input-group-text">Taille (1,26m à 3m)</span>
+                <input type="number" min="1.26" max="3" step="0.01" class="form-control" name="taille" id="form-taille" placeholder="1.7" required>
+                 <span class="input-group-text">m</span>
+            </div>
+         </div>
+         <div class="form-check form-check-inline">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="sex" id="sex-Femme" value="Femme" required>
+              Femme
+            </label>
+         </div>
+         <div class="form-check form-check-inline">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="sex" id="sex-Homme" value="Homme" required>
+              Homme
+            </label>
+         </div>
 </div> 
 <div class="card col-md-4 mx-auto my-1">
 <header class="text-start">Connaissances</header>
@@ -65,10 +100,10 @@ include("includes/form.inc.html");
 <div class="card col-11 mx-auto my-1">
     <div class="form-group">
       <label for="formFile" class="form-label mt-4"><p>joindre une image(jpg ou png)</p></label>
-      <input class="form-control" type="file" id="formFile">
+      <input class="form-control" type="file" name="img" id="img">
     </div>
 </div>
 <div class="d-flex flex-row-reverse bd-highlight mt-4">
          <button type="submit" name="save" class="btn btn-primary">Enregistrer les données</button>
-</div>
-
+        </div>
+</form>        
